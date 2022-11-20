9da24c7e31eb8cedae9d93ff6cd977b2df4e3c80
gustavoeyros <gustavoeyros@hotmail.com>
2022-11-19 22:04:27 -0300

    user update

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -137,22 +137,29 @@ module.exports = class UserController {
           return
          }
          user.email = email
-
-         if(!password){
-          res.status(422).json({message: 'A senha é obrigatória!'})
-          return
-         }
-         user.password = password
          if(!phone){
           res.status(422).json({message: 'O telefone é obrigatório!'})
           return
          }
          user.phone = phone
-         if(!confirmpassword){
-          res.status(422).json({message: 'A confirmação de senha é obrigatória'})
+         if(password != confirmpassword){
+          res.status(422).json({message:'As senhas não conferem!'})
           return
+         } else if(password == confirmpassword && password != null){
+           //create a password 
+          const salt = await bcrypt.genSalt(12) 
+          const hashedPassword = await bcrypt.hash(password, salt)
+          user.password = hashedPassword
          }
-         user.confirmpassword = confirmpassword
+        
+      try {
+          //return user updated data
+          await User.findOneAndUpdate({_id: user.id}, {$set: user}, {new: true})
+          res.status(200).json({message: 'Usuário atualizado com sucesso!'})
+      } catch (error) {
+          res.status(500).json({message: error})
+          return
+      }
 
 
      
