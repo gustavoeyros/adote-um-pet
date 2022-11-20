294516d2a8f384ec2fab766bd0c00108a4140434
gustavoeyros <gustavoeyros@hotmail.com>
2022-11-20 20:38:50 -0300

    image upload

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -117,7 +117,10 @@ module.exports = class UserController {
      const user = await getUserByToken(token)
 
      const {name, email, phone, password, confirmpassword} = req.body
-     let image = ''
+
+     if(req.file){
+          user.image = req.file.filename
+     }
 
      
      //validations
