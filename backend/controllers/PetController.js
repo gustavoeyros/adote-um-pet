f80e01620b18e51bce2e2f353ccd9adff326751e
gustavoeyros <gustavoeyros@hotmail.com>
2022-11-27 15:18:10 -0300

    remove pet by id

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -94,5 +94,28 @@ module.exports = class PetController{
         }
         res.status(200).json({pet: pet})
     }
+    static async removePetById (req, res){
+        const id = req.params.id
+        if(!ObjectId.isValid(id)){
+            res.status(422).json({messag: 'ID inválido!'})
+            return
+        }
+        const pet = await Pet.findOne({_id: id})
+        if(!pet){
+            res.status(404).json({message: 'Pet não encontrado'})
+            return
+        }
+
+        //check if logged user registered pet
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        if(pet.user._id.toString() !== user._id.toString()){
+            res.status(422).json({message: 'Ocorreu um problema, tente novamenta mais tarde!'})
+            return
+        }
+        await Pet.findByIdAndRemove(id)
+        res.status(200).json({message:'Pet removido com sucesso!'})
+    }
 
 }
